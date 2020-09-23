c9cfc67220d0f79d2e54a636984688f790e9e7d5
Yurii Fostiak <37013714+DRAGON1662@users.noreply.github.com>
2020-09-23 23:58:53 +0300

    edited homework1

--- programming/task1/main.py
+++ programming/task1/main.py
@@ -1,5 +1,3 @@
-amount = int(input("How many numbers? "))
-
 def common_luca_numbers(): 
   n1, n2 = 2, 1
   count = 0
@@ -8,14 +6,24 @@ def common_luca_numbers():
   elif amount == 1:
     print("First Luka number -", n1)
   else:
-    print("Luka sequence:")
+    print("Prime Luka sequence:")
     while count < amount:
-        if n1 != 1 and all(n1%i!=0 for i in range(2,n1)):
+        if n1 != 1 and all( n1%i!=0 for i in range(2,n1)):
           print(n1)
         nth = n1 + n2
       # update values
         n1 = n2
         n2 = nth
         count += 1
+
+while True:
+    try:
+      amount = int(input("How many numbers? "))
+      if amount <= 0:
+        print("Enter a positive integer")
+        continue
+      break
+    except:
+        print('It must be an amount of numbers from 1')
 
 common_luca_numbers()
