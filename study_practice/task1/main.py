6cd84b3d5b6a8d5df51e85350fa8e8e1f3c17653
Yurii Fostiak <37013714+DRAGON1662@users.noreply.github.com>
2020-09-23 23:59:41 +0300

    edited homework1

--- study_practice/task1/main.py
+++ study_practice/task1/main.py
@@ -1,13 +1,23 @@
-n = int(input('Enter n: '))
-if n%2 == 0 and n <= 100 and n >= 0:
+def check_lucky_ticket():
   amount = 0
-  for a in range(10**(n-1), 10**n):
-      a = list(str(a))
-      a1 = [int(i) for i in a[:len(a)//2]]
-      a2 = [int(i) for i in a[len(a)//2:]]
+  for a in range(10**n):
+    a = list(str(a).zfill(n))
+           
+    a1 = [int(i) for i in a[:len(a)//2]]
+    a2 = [int(i) for i in a[len(a)//2:]]
+    if sum(a1) == sum(a2):
+      amount += 1 
+  print('The amount of lucky tickets', amount)
 
-      if sum(a1) == sum(a2):
-          amount += 1 
-  print('The amount of lucky tickets ', amount)
-else:
-  print('Your number from 2 to 100 must be odd!')
+while True:
+    try:
+      n = int(input('Enter n: '))
+      if n%2 != 0:
+        print('Number must be odd!')
+        continue
+      break
+    except:
+        print('Your number from 2 to 100 must be odd!')
+
+check_lucky_ticket()
+
